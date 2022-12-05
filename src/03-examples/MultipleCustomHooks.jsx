c0716da168b1715e15fa548d0c06aba279a2a5ab
enrique <enriquealejo28@gmail.com>
2022-12-05 16:58:48 -0600

    cambio de if a ternario

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -8,23 +8,30 @@ export const MultipleCustomHooks = () => {
   // https://www.breakingbadapi.com/api/quotes/1 cambiar cuando ya funcione
 
   // console.log({ data, isLoading, hasError });
-  if(isLoading){
-    return(<h1>Loading...</h1>)
-  }
+  // if(isLoading){
+  //   return(<h1>Loading...</h1>)
+  // }
   
   return(     
   <>
     <h1>Breaking Quotes</h1>
     <hr />
-
-    <div className='alert alert-info text-center'>
-      Loading 
+    {
+      isLoading 
+      ?(
+        <div className='alert alert-info text-center'>
+        Loading 
     </div>
+      ) 
+      :(
+      <blockquote className='blockquote text-end'>
+        <p className='mb-1'>Hola mundo</p>
+        <footer className='blockquote-footer'>Enrique alejo</footer>
+      </blockquote>
+      )
+    }
 
-    <blockquote className='blockquote text-end'>
-      <p className='mb-1'>Hola mundo</p>
-      <footer className='blockquote-footer'>Enrique alejo</footer>
-    </blockquote>
+
 
   </>
   );
